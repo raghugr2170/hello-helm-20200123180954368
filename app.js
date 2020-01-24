b74cb98c36e80b615d4c270ec3cfdb42e5c79200
raghugr2170 <52022445+raghugr2170@users.noreply.github.com>
2020-01-24 10:32:11 +0530

    Update app.js
    Version 3

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.PORT) {
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Welcome to IBM Cloud DevOps with Docker, Kubernetes and Helm Charts. Lets go use the Continuous aitha Delivery Service ,version 2');
+  res.send('Welcome to IBM Cloud DevOps with Docker, Kubernetes and Helm Charts. Lets go use the Continuous aitha Delivery Service ,version 3');
 });
 
 app.listen(PORT);
